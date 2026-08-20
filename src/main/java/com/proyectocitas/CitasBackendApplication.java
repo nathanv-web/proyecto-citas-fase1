c46549f4befcb65eb9a5c94e024b2ee2ecdb01cb
natha <natha@Nathan>
2026-08-19 19:00:33 -0600

    Módulo de Médicos y Especialidades

---
+++ src/main/java/com/proyectocitas/CitasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.proyectocitas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitasBackendApplication.class, args);
+	}
+
+}
